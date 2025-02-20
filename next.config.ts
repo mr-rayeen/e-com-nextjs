c6f839aa61ef02493e2f031205f444b33ca1be3f
Salman Rayeen <mohdsr242@gmail.com>
2025-02-20 15:35:42 +0530

    Update Next.js configuration to allow ESLint errors during production builds and format image domains

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
-		domains: ["cdn.sanity.io", "www.animatedimages.org","img.icons8.com"],
+		domains: [
+			"cdn.sanity.io",
+			"www.animatedimages.org",
+			"img.icons8.com",
+		],
+	},
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
 	},
 };
 
