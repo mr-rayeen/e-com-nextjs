8b80dab2424fbcbb50a4b789096fd9d634871334
Salman Rayeen <mohdsr242@gmail.com>
2025-02-20 17:25:00 +0530

    Update Next.js configuration to allow production builds with type errors and add ESLint configuration for TypeScript support

--- next.config.ts
+++ next.config.ts
@@ -9,11 +9,16 @@ const nextConfig: NextConfig = {
 			"img.icons8.com",
 		],
 	},
-	eslint: {
-		// Warning: This allows production builds to successfully complete even if
-		// your project has ESLint errors.
-		ignoreDuringBuilds: true,
+	typescript: {
+		// !! WARN !!
+		// Dangerously allow production builds to successfully complete even if
+		// your project has type errors.
+		// !! WARN !!
+		ignoreBuildErrors: true,
 	},
+	eslint:{
+    ignoreDuringBuilds: true, //during builds
+  }
 };
 
 export default nextConfig;
