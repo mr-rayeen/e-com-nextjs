cea9da30eba4b655f7d2a0681021f00afa57a914
Salman Rayeen <mohdsr242@gmail.com>
2025-02-20 15:49:13 +0530

    Update ESLint configuration to extend TypeScript support and disable specific rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,11 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.rules({
+		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/no-unused-vars": "off",
+	}),
 ];
 
 export default eslintConfig;
